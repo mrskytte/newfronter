3d5fd807f4c080a7369386ff5242fbff37927ed9
Christoffer Skytte Wielsøe <FatherofPiper@Marys-MacBook-Pro.local>
2019-05-29 23:21:24 +0200

    Folder open and closing added in JS

--- KEA-prototype.js
+++ KEA-prototype.js
@@ -90,3 +90,85 @@ function openMenu() {
 //Main
 
 //Recent Document
+
+//TM Folder
+const tm = document.getElementById("tm")
+const tmhead = document.getElementById("tmhead")
+const tm1 = document.getElementById("tm1")
+const tm2 = document.getElementById("tm2")
+const tm3 = document.getElementById("tm3")
+const tm4 = document.getElementById("tm4")
+const tm5 = document.getElementById("tm5")
+const tm6 = document.getElementById("tm6")
+const tm7 = document.getElementById("tm7")
+const tmArrow = document.getElementById("tmarrow")
+
+tmhead.addEventListener("click", openTM)
+
+function openTM() {
+    if (tmArrow.classList == "arrow arrowspin") {
+        tmArrow.classList.remove("arrowspin")
+        tmArrow.classList.add("reversespin")
+        tm.classList.add("closetm")
+        tm.classList.remove("opentm")
+        tm1.classList.add("closetm1")
+        tm1.classList.remove("opentm1")
+        tm2.classList.add("closetm2")
+        tm2.classList.remove("opentm2")
+        tm3.classList.add("closetm3")
+        tm3.classList.remove("opentm3")
+        tm4.classList.add("closetm4")
+        tm4.classList.remove("opentm4")
+        tm5.classList.add("closetm5")
+        tm5.classList.remove("opentm5")
+        tm6.classList.add("closetm6")
+        tm6.classList.remove("opentm6")
+        tm7.classList.add("closetm7")
+        tm7.classList.remove("opentm7")
+    } else {
+        tmArrow.classList.add("arrowspin")
+        tmArrow.classList.remove("reversespin")
+        tm.classList.remove("closetm")
+        tm.classList.add("opentm")
+        tm1.classList.remove("closetm1")
+        tm1.classList.add("opentm1")
+        tm2.classList.remove("closetm2")
+        tm2.classList.add("opentm2")
+        tm3.classList.remove("closetm3")
+        tm3.classList.add("opentm3")
+        tm4.classList.remove("closetm4")
+        tm4.classList.add("opentm4")
+        tm5.classList.remove("closetm5")
+        tm5.classList.add("opentm5")
+        tm6.classList.remove("closetm6")
+        tm6.classList.add("opentm6")
+        tm7.classList.remove("closetm7")
+        tm7.classList.add("opentm7")
+    }
+}
+
+//EX folder
+const exHead = document.getElementById("exhead")
+const ex = document.getElementById("ex")
+const ex1 = document.getElementById("ex1")
+const exArrow = document.getElementById("exarrow")
+
+exHead.addEventListener("click", openEX)
+
+function openEX() {
+    if (exArrow.classList == "arrow arrowspin") {
+        exArrow.classList.remove("arrowspin")
+        exArrow.classList.add("reversespin")
+        ex.classList.add("closeex")
+        ex.classList.remove("openex")
+        ex1.classList.add("closeex1")
+        ex1.classList.remove("openex1")
+    } else {
+        exArrow.classList.add("arrowspin")
+        exArrow.classList.remove("reversespin")
+        ex.classList.remove("closeex")
+        ex.classList.add("openex")
+        ex1.classList.remove("closeex1")
+        ex1.classList.add("openex1")
+    }
+}
