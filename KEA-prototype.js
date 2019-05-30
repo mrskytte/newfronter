25a434c0d39042edff3aa30935b831fd2aea6170
Christoffer Skytte Wielsøe <FatherofPiper@Marys-MacBook-Pro.local>
2019-05-31 01:54:58 +0200

    JS file divided in index and main page.. Folders on mainpage sorted nicely

--- KEA-prototype.js
+++ KEA-prototype.js
@@ -1,22 +1,3 @@
-//Login page
-
-//const username = document.getElementById("username")
-//const password = document.getElementById("password")
-//const usernameTxt = document.getElementById("usernametxt")
-//const passwordTxt = document.getElementById("passwordtxt")
-//
-//username.addEventListener("click", moveUsername)
-//password.addEventListener("click", movePassword)
-//
-//function moveUsername(){
-//    usernameTxt.classList.add("moveinput")
-//}
-//
-//function movePassword(){
-//    passwordTxt.classList.add("moveinput")
-//}
-//
-
 //Main page
 
 //Header
